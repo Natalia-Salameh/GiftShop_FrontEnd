3359fff1349b3da0e717a003598a7bbb05d24490
shahdghayada <shahedsa3d.2002.s@gmail.com>
2023-05-27 11:12:11 +0300

    product Details

--- src/pages/productDetails/index.jsx
+++ src/pages/productDetails/index.jsx
@@ -1,33 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
-import './style.css'
+import './style.css';
 
 const ProductDetails = () => {
-    const {id} = useParams();
-  
-  
-    const [product, setProduct] = useState([])
-    useEffect(()=>{ 
-       fetch(`http://localhost:8080/products/id/${id}`).then(res=>res.json()).then(data=>setProduct(data))
-  },[])
-  
- 
-  return (
-      <div className='div'>
-        <h1 className='h1'>Product Details </h1>
-        <div className="product-image">
-          <img  className='img' src={product.image} />
-        </div>
-        <div className='div'>
-          <h3 className='h3'>{product.title}</h3>
-          <p className='p'>{product.description} </p>
-          <p className='p2'>{product.category} </p>
-          <p className='p3'>{product.price} $</p>
-          <p className='p2'>rate : {product?.rating?.rate} count : {product?.rating?.count} </p>
-        </div>
-  
-      </div>
+  const { id } = useParams();
+  const [product, setProduct] = useState({});
 
-  )};
-  export default ProductDetails;
+  useEffect(() => {
+    fetch(`http://localhost:8080/products/id/${id}`)
+      .then(res => res.json())
+      .then(data => setProduct(data));
+  }, []);
+
+  const squareStyle = {
+    width: '15px',
+    height: '15px',
+    backgroundColor: product.colour,
+    display: 'inline-block',
+    marginRight: '10px' // Adding some spacing between the square and text
+  };
+
+  return (
+    <>
+      <div className="contai">
+        <div className="images">
+          <img className='img' src={product.image} alt={product.name} />
+        </div>
+        <div className="slideshow-buttons">
+          <div className="one"></div>
+          <div className="two"></div>
+          <div className="three"></div>
+          <div className="four"></div>
+        </div>
+        <div className="product">
+          <h1 className='h1'>{product.name}</h1>
+          <h2 className='h2'>${product.price}</h2>
+          <h2 className='h3'>
+          Product colour: <span className="square" style={squareStyle}></span>
+        </h2>          <p className="desc">{product.description}</p>
+          <div className="buttons">
+            <button className="add">Add to Cart</button>
+            <button className="like"><span>♥</span></button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductDetails;
