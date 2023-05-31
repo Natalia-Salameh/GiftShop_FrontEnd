bc82eb528af39c8aa16c379145e0d25c2a1b8b5a
shahdghayada <shahedsa3d.2002.s@gmail.com>
2023-05-31 20:03:08 +0300

    commit

--- src/components/productCard/index.jsx
+++ src/components/productCard/index.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-const ProductCard = ({ productList , id}) => {
+import { CartContext } from '../../components/AddToCart/cartContext';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const ProductCard = ({ productList ,id}) => {
+  // const { id } = useParams();
+  // const [product, setProduct] = useState({});
+  const { addToCart } = useContext(CartContext);
 
   const styles = {
     backgroundImage: `url(${productList.image})`,
@@ -8,24 +15,46 @@ const ProductCard = ({ productList , id}) => {
     backgroundSize: 'cover',
    
   };
+
+  const handleAddToCart = () => {
+    addToCart({ ...productList, quantity: 1 });
+    toast.success('Product added to cart successfully!', {
+      position: toast.POSITION.TOP_RIGHT
+    });
+  };
+  
   
   return (
-    <Link to={`/products/id/${productList.id}`}>
-<div className="container" style={styles}>
+//     <Link to={`/products/id/${productList.id}`}>
+// <div className="container" style={styles}>
 
-      <div className="overlay">
-        <h2 className="head">{productList.name}</h2>
-        <hr />
-        <p className="price">
-        {productList.price}$  
-            </p>
-        <div className="cart">
-          <i className="fas fa-shopping-cart"></i>
-          <button>Add to Cart</button>
-        </div>
-      </div>
-    </div>
-    </Link>
+//       <div className="overlay">
+//         <h2 className="head">{productList.name}</h2>
+//         <hr />
+//         <p className="price">
+//         {productList.price}$  
+//             </p>
+//         <div className="cart">
+//           <i className="fas fa-shopping-cart"></i>
+//           <button onClick={handleAddToCart}>Add to Cart</button>
+//         </div>
+//       </div>
+//     </div>
+//     </Link>
+
+<div className="container" style={styles}>
+<div className="overlay">
+  <Link to={`/products/id/${productList.id}` } style={{ textDecoration: "none" , color: "inherit" }}>
+    <h2 className="head">{productList.name}</h2>
+    <hr />
+    <p className="price">{productList.price}$</p>
+  </Link>
+  <div className="cart">
+    <i className="fas fa-shopping-cart"></i>
+    <button onClick={handleAddToCart}>Add to Cart</button>
+  </div>
+</div>
+</div>
   );
 };
 
