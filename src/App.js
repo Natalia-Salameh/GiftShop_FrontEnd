34d346b6cddb911ebafa30e44e9176f050cd5a21
shahdghayada <shahedsa3d.2002.s@gmail.com>
2023-05-31 22:28:31 +0300

    Checkout

--- src/App.js
+++ src/App.js
@@ -17,75 +17,79 @@ import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Login/signup";
 import Cart from "./components/AddToCart/Cart";
 import { CartProvider } from "./components/AddToCart/cartContext";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Slide } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Slide } from "react-toastify";
 import CategoryPage from "./pages/Admin/categoryPage";
 import ProductPageAdmin from "./pages/Admin/productPage";
 import AddNewCategory from "./pages/Admin/addCategory/add_new_categoryPage";
 import AddNewProduct from "./pages/Admin/addProduct/addProductPage";
+import CheckoutPage from "./pages/orderPage";
 
 function App() {
   return (
     <div className="App">
       <AuthContextProvider>
-         <CartProvider>
-        <BrowserRouter>
-          <ToastContainer transition={Slide} />
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/candles" element={<Candles />} />
-            <Route path="/ceramic" element={<Ceramic />} />
-            <Route path="/crochet" element={<Crochet />} />
-            <Route path="/embroidery" element={<Embroidery />} />
-            <Route path="/resin" element={<Resin />} />
-            <Route path="/woodArt" element={<WoodArt />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/products/id/:id" element={<ProductDetails />} />
-            <Route path="/products/name/:name" element={<ProductPage />} />
-             <Route exact path="/cart" component={Cart} />
+        <CartProvider>
+          <BrowserRouter>
+            <ToastContainer transition={Slide} />
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/candles" element={<Candles />} />
+              <Route path="/ceramic" element={<Ceramic />} />
+              <Route path="/crochet" element={<Crochet />} />
+              <Route path="/embroidery" element={<Embroidery />} />
+              <Route path="/resin" element={<Resin />} />
+              <Route path="/woodArt" element={<WoodArt />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/products/id/:id" element={<ProductDetails />} />
+              <Route path="/products/name/:name" element={<ProductPage />} />
+              <Route exact path="/cart" component={Cart} />
 
+              <Route
+                path="categorypage/Categories/:id/products"
+                element={
+                  <div>
+                    <ProductPageAdmin />
+                  </div>
+                }
+              />
 
-             <Route path="categorypage/Categories/:id/products" element={
-            <div>
-              {/* <Navbar /> */}
-            <ProductPageAdmin/>
-            {/* <Footer /> */}
-            </div>
-            } 
-            />
+              <Route
+                path="/newcategory"
+                element={
+                  <div>
+                    <AddNewCategory />
+                  </div>
+                }
+              />
 
-            <Route path="/newcategory" element={<div>
-              {/* <Navbar /> */}
-              <AddNewCategory/>
-              {/* <Footer /> */}
+              <Route
+                path="/newProduct"
+                element={
+                  <div>
+                    <AddNewProduct />
+                  </div>
+                }
+              />
 
-            </div>} />
+              <Route
+                exact
+                path="/categorypage"
+                element={
+                  <div>
+                    <CategoryPage />
+                  </div>
+                }
+              />
+              <Route exact path="/checkout" element={<CheckoutPage />} />
+            </Routes>
 
-            <Route path="/newProduct" element={<div>
-              {/* <Navbar /> */}
-              <AddNewProduct/>
-              {/* <Footer /> */}
-
-            </div>} />
-
-            <Route exact path="/categorypage"  element={
-              <div>
-                {/* <Navbar /> */}
-                {/* <Home/> */}
-                <CategoryPage />
-                {/* <Footer /> */}
-              </div>
-            } />
-
-            
-          </Routes>
-         
-          <Footer />
-        </BrowserRouter>
+            <Footer />
+          </BrowserRouter>
         </CartProvider>
       </AuthContextProvider>
     </div>
